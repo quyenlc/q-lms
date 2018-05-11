2be1b146f80af71d21303f93c5905e488d3a098b
Tung Thanh Vu <tung.vu@punch.vn>
2018-05-11 11:07:56 +0700

    Config social_django

--- settings/base.py
+++ settings/base.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
